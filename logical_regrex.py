568b48d4f42a3bf7bc83f9abb3746f12f7ccf733
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2024-01-16 16:11:46 -0600

    initial commit

---
+++ logical_regrex.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+def sF(x):
+    '''
+    Represents the sigmoid function
+    S(x) = 1/(1+e^[-x])
+    '''
+    Sx = 1/(1+np.exp(-x))
+    return Sx
+
+class logical_Rgrex:
+    def __init__(self,L=0.01, n=2000):
+        '''
+        L: learining rate 
+        n: number of iterations to do
+        '''
+        self.l_rate = L
+        self.iterate = n 
+        self.weights = None
+        self.bias = None
+    
+    
+    def fit_function(self,X,y):
+        '''
+        X: data as input
+        y: the expected value for the Xi value
+        Initializes de fitting of the model with bias as 0, weights as 0
+        Sets the features and samples from the input data
+        Function implied : y(x) = 1 / 1 + e^(-wx +b)
+        '''
+
+        features, samples = X.shape
+        self.weights = np.zeros(features) #No. of weights = No. Features
+
+        #start the training for the machine
+        for _ in range(self.iterate):
+            power = np.dot(X,self.weights) + self.bias
+            output = sF(power)
+            #calculate the error and adjust the weights 'n bias accordingly 
+            
+            dw = 1/samples*np.dot(X.T , (output - y))
+            db = 1/samples*np.sum(output - y)
+            
+            self.weights = self.weights - self.l_rate*dw
+            self.bias = self.bias - self.l_rate*db
+
+
+    def predict(self, X):
+            power = np.dot(X,self.weights) + self.bias
+            output = sF(power)
+            return [0 if Oi  <= 0.5 else 1 for Oi in output ]
